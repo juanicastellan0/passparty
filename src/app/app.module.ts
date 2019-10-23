be383b8221dc88852bb2c7273f49422a2b2b740e
juan <juanicastellano14@gmail.com>
2019-10-23 15:34:15 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ApplicationsComponent } from './applications/applications.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ApplicationsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/applications/applications.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-applications',
+  templateUrl: './applications.component.html',
+  styleUrls: ['./applications.component.css']
+})
+export class ApplicationsComponent implements OnInit {
+  private multinexo: string;
+
+  constructor() {
+    this.multinexo = 'Multinexo';
+  }
+
+  ngOnInit() {
+  }
+
+}
